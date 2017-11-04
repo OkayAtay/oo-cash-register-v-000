dc51f3a8ec36948a0ba7b2796e335c67b3107774
Araba Taylor <araba.taylor20@gmail.com>
2017-11-04 22:49:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+require 'pry'
+class CashRegister
+attr_accessor :total, :items_array
+@@items_array = []
 
+  def initialize(n=0)
+      @total=n
+  end
+
+  def discount
+    self.total = 20
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(item, price, quantity = 1)
+    #binding.pry
+    @@items_array << item
+    self.total += price*quantity
+
+  end
+
+  def apply_discount
+    puts "After the discount, the total comes to #{}"
+  end
+
+  def items
+    @@items_array
+  end
+
+end
