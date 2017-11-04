ac722c6e3ff2354c2f3f9f9f2ee23c5bf441611d
Araba Taylor <araba.taylor20@gmail.com>
2017-11-04 23:49:15 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,9 +23,10 @@ attr_accessor :total, :items_array, :discount
   def apply_discount
     if @discount == 0
       "There is no discount to apply."
+    else 
+      @total = @total - (@total*@discount*0.01)
+      "After the discount, the total comes to $#{@total.to_i}."
     end
-    @total = @total - (@total*@discount*0.01)
-    "After the discount, the total comes to $#{@total.to_i}."
   end
 
   def items
