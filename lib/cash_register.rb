681dafc8c1e93f0b299b897e2015afd8ccefc72c
Araba Taylor <araba.taylor20@gmail.com>
2017-11-04 23:22:30 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,11 @@
 require 'pry'
 class CashRegister
-attr_accessor :total, :items_array
+attr_accessor :total, :items_array, :discount
 @@items_array = []
 
   def initialize(n=0)
       @total=n
+      @discount=
   end
 
   def discount
