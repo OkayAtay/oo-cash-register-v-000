c8da90bf4fd3b968268ad137af64597dbcb12f4e
Araba Taylor <araba.taylor20@gmail.com>
2017-11-04 23:46:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,7 +22,6 @@ attr_accessor :total, :items_array, :discount
 
   def apply_discount
     @total = @total - (@total*@discount*0.01)
-     @total*".8".to_f
     puts "After the discount, the total comes to #{@total}"
   end
 
