8c65c4723d91647ca53889ff12017149889539e4
Araba Taylor <araba.taylor20@gmail.com>
2017-11-05 01:02:14 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ attr_accessor :total, :items_array, :discount, :most_recent_price
   def add_item(item, price, quantity=1)
     @most_recent_price = price
     (@items_array << item)*quantity
-    @total += price*quantity
+    @total += @most_recent_price*quantity
   end
 
   def apply_discount
