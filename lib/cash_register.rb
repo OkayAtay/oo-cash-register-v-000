315d2bbbdc89011f50713d1169c09526ce500ec1
Araba Taylor <araba.taylor20@gmail.com>
2017-11-05 00:26:51 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,6 @@ attr_accessor :total, :items_array, :discount
   end
 
   def add_item(item, price, quantity = 1)
-    binding.pry
       (@items_array << item)*quantity
     self.total += price*quantity
 
